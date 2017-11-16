4886e4b4b381f74f935a97ce46c4d923f2b8273a
Justin Reid <justin.reid@skookum.com>
2017-11-16 16:14:38 -0500

    Make text pivot point center bottom of the text

--- TestAR/ViewController.swift
+++ TestAR/ViewController.swift
@@ -117,7 +117,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let text = SCNNode(geometry: textGeometry)
         text.position = SCNVector3Make(0, scaledRadius + 0.01, 0)
         text.scale = SCNVector3Make(0.01, 0.01, 0.01)
-        centerOfMass.addChildNode(text)
+        
+        // Make the text anchor point center bottom
+        let (textMin, textMax) = textGeometry.boundingBox
+        text.pivot = SCNMatrix4MakeTranslation(textMin.x + (textMax.x - textMin.x) * 0.5, 0, 0)
+        
+        // We can not put the billboard constraint directly onto a text node
+        // Because Apple is broken
+        // So this parent node only exists to hold the constraint
+        let textParentNode = SCNNode()
+        textParentNode.addChildNode(text)
+        
+        let directionConstraint = SCNBillboardConstraint()
+        textParentNode.constraints = [directionConstraint]
+        
+        centerOfMass.addChildNode(textParentNode)
+        
         
         let sphere = SCNNode(geometry: SCNSphere(radius: CGFloat(scaledRadius)))
         sphere.geometry?.materials = [material]
